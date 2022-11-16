b6627e88fb903987e38ec1eaa9a8e4fba5f644a2
Andreia Vieira <andreiavieira2353@gmail.com>
2022-11-16 16:07:09 -0300

    Validate Password

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ import dotenv from "dotenv";
 import nodemon from 'nodemon'
 import joi from "joi";
 import { MongoClient } from 'mongodb'
-import { v4 as uuid } from 'uuid';
+import { v4 as uuidV4 } from 'uuid';
 import bcrypt from 'bcrypt'
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 dotenv.config();
-const token = uuid();
 
 const userSchema = joi.object({
   name: joi.string().required().min(3).max(100),
   email: joi.string().email().required(),
 password: joi.string().required(),
+validatePass: joi.string().required(),
 })
 
 const mongoClient =  new MongoClient(process.env.MONGO_URI);
@@ -29,35 +29,40 @@ try {
 }
 const db = mongoClient.db("myWallet");
 const userCollection = db.collection("users")
-
-  app.post("/sign-up", async (req,res) => {
-    const user = req.body;
-    try{
-      const userExists = await userCollection.findOne({email: user.email})
+const sessionColletion = db.collection("session")
+app.post("/sign-up", async (req,res) => {
+  const user = req.body;
+  try{
+    const userExists = await userCollection.findOne({email: user.email})
     if(userExists){
       return res.status(409).send({message : "Esse email já existe"})
     }
-
+    
     const {error} = userSchema.validate(user, {abortEarly: false})
-if(error){
-  const errors = error.details.map(detail => detail.message);
-  return res.status(400).send(errors)
-}
-const hashPassword = bcrypt.hashSync(user.password, 10)
-console.log(hashPassword)
-
-
-await userCollection.insertOne({...user, password: hashPassword})
-res.sendStatus(201);
-}catch (err) {
+    if(error){
+      const errors = error.details.map(detail => detail.message);
+      return res.status(400).send(errors)
+    }
+    if(user.validatePass !== user.password){
+      return res.status(409).send({message : "Senha incorreta"})
+    }
+    const hashPassword = bcrypt.hashSync(user.password, 10)
+    console.log(hashPassword)
+    
+    
+    await userCollection.insertOne({...user, password: hashPassword})
+    res.sendStatus(201);
+  }catch (err) {
   console.log(err)
   res.sendStatus(500)
 }
 
-  })
+})
 
-  app.post("/sign-in" ,async (req, res)=> {
-    const {email, password} = req.body;
+app.post("/sign-in" ,async (req, res)=> {
+  const {email, password} = req.body;
+  const token = uuidV4();
+  console.log(token)
 try{
 const userExists = await userCollection.findOne({email})
 if(!userExists){
@@ -70,7 +75,11 @@ if(!passwordOk){
   return res.sendStatus(401);
 }
 
-res.send({message: `Olá ${userExists.name}, seja bem vindo!`})
+await sessionColletion.insertOne({
+  token, 
+  userId: userExists._id
+})
+res.send({token})
 } catch (err) {
   console.log(err)
   res.sendStatus(500)
